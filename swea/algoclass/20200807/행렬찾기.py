e50ee58d237110f1356580e05b5c77ee7aa96f56
KGB <KGB>
2020-08-03 21:29:47 +0900

    swea 20200803

--- swea/algoclass/20200807/행렬찾기.py
+++ swea/algoclass/20200807/행렬찾기.py
@@ -1,27 +1,20 @@
-b=[]
-def width(i, j):
-    c=1;d=j
-    while 1:
-        d+=1
-        if d>=n:break
-        if b[i][d]:c+=1
-        else:break
-    return c
-def height(i,j):
-    c=1;d=i
-    while 1:
-        d+=1
-        if d>=n:break
-        if b[d][j]:c+=1
-        else:break
-    return c
-
 for t in range(int(input())):
     n=int(input());b=[list(map(int,input().split())) for _ in range(n)];a=[]
     for i in range(n):
         for j in range(n):
             if b[i][j]:
-                w=width(i,j);h=height(i,j)
+                w=1;d=j
+                while 1:
+                    d+=1
+                    if d>=n:break
+                    if b[i][d]:w+=1
+                    else:break
+                h=1;d=i
+                while 1:
+                    d+=1
+                    if d>=n:break
+                    if b[d][j]:h+=1
+                    else:break
                 for k in range(i,i+h):
                     for l in range(j,j+w):
                         b[k][l]=0
