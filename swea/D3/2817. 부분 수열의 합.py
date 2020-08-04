e33d10e8e72d530f1264b38e419cd3ad8ee9f560
KGB <kbsam2@gmail.com>
2020-08-04 12:01:22 +0900

    swea 20200804

--- swea/D3/2817. 부분 수열의 합.py
+++ swea/D3/2817. 부분 수열의 합.py
@@ -1,4 +1,5 @@
 def sol(total,pos):
+    global c
     if total > k:return
     if total == k:
         c+=1
@@ -8,9 +9,9 @@ def sol(total,pos):
     sol(total+a[pos],pos+1)
     
 for t in range(int(input())):
-    global n,k,a,c
+    global n,k,a
     c=0
     n,k=map(int,input().split())
     a=list(map(int,input().split()))
     sol(0,0)
-    print(c)
+    print(f'#{t+1}',c)
