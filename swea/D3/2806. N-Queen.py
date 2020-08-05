b2ef6b9e5cf1ae9479055dbe7eef57a9561f4118
KGB <KGB>
2020-08-05 22:06:27 +0900

    update

--- swea/D3/2806. N-Queen.py
+++ swea/D3/2806. N-Queen.py
@@ -4,12 +4,12 @@ def sol(k, used):
     for i in range(n):
         if used&(1<<i):continue
         for j in range(k):
-            if k-j==abs(i-col[j]):break
+            if k-j==abs(i-c[j]):break
         else:
-            col[k]=i
+            c[k]=i
             sol(k+1,used|(1<<i))
 
 for t in range(int(input())):
-    n=int(input());cnt=0;col=[0]*n
+    n=int(input());cnt=0;c=[0]*n
     sol(0,0)
     print(f'#{t+1}',cnt)
