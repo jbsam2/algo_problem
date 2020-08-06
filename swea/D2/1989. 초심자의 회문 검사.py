ed50ca2ada37aff96aec24c0d0996b33d92ac85c
KGB <kbsam2@gmail.com>
2020-08-06 15:48:54 +0900

    update

--- swea/D2/1989. 초심자의 회문 검사.py
+++ swea/D2/1989. 초심자의 회문 검사.py
@@ -1,5 +1,4 @@
 for t in range(int(input())):
-    w=input()
-    for i in range(len(w)//2):
-        if w[i]!=w[-1-i]:print(f'#{t+1} 0');break
-    else:print(f'#{t+1} 1')
+    word=input()
+    if word==word[::-1]:print(f'#{t+1}',1)
+    else:print(f'#{t+1}',0)
