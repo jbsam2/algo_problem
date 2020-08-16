a329fb85fcdfb158a2ef96e517eff47085ccd130
KGB <kbsam2@gmail.com>
2020-08-16 21:07:01 +0900

    update

--- swea/D4/5432. 쇠막대기 자르기.py
+++ swea/D4/5432. 쇠막대기 자르기.py
@@ -1,8 +1,4 @@
 for t in range(int(input())):
     a=c=k=0
-    for i in input():
-        j=')'>i
-        c+=2*j-1
-        a+=(j or c)*k
-        k=j        
+    for i in input():j=')'>i;c+=2*j-1;a+=(j or c)*k;k=j        
     print(f'#{t+1}',a)
