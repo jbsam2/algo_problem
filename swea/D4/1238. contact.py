14eaf4b9c8197b0d05d4a61fcbc6ef9aee411ffd
KGB <kbsam2@gmail.com>
2020-08-21 22:21:14 +0900

    swea

--- swea/D4/1238. contact.py
+++ swea/D4/1238. contact.py
@@ -1,9 +1,9 @@
 for t in range(10):
-    V=[[]for _ in range(101)];l,s=map(int,input().split());q=[[s]];v=[s];D=[*map(int,input().split())]
-    for i in range(l//2):V[D[2*i]]+=[D[2*i+1]]
+    c=[[]for _ in range(101)];l,s=map(int,input().split());q=[[s]];v=[s];D=[*map(int,input().split())]
+    for i in range(l//2):c[D[2*i]]+=[D[2*i+1]]
     while q[-1]:
         q+=[[]]
         for i in q[-2]:
-            for j in V[i]:
+            for j in c[i]:
                 if j not in v:v+=[j];q[-1]+=[j]
     print(f'#{t+1}',max(q[-2]))
