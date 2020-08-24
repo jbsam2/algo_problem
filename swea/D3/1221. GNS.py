5931636f6f82fb4c00c669b2dcab9c6020cbbb9f
KGB <kbsam2@gmail.com>
2020-08-24 20:11:42 +0900

    swea

--- swea/D3/1221. GNS.py
+++ swea/D3/1221. GNS.py
@@ -1,8 +1,7 @@
-d={"ZRO":0,"ONE":1,"TWO":2,"THR":3,"FOR":4,"FIV":5,"SIX":6,"SVN":7,"EGT":8,"NIN":9}
-e={0:"ZRO",1:"ONE",2:"TWO",3:"THR",4:"FOR",5:"FIV",6:"SIX",7:"SVN",8:"EGT",9:"NIN"}
+d=["ZRO","ONE","TWO","THR","FOR","FIV","SIX","SVN","EGT","NIN"]
 for t in range(int(input())):
-    k=input();n=list(input().split());r=[]
-    for i in n:r.append(d[i])
+    input();n=list(input().split());r=[0]*10
+    for i in n:r[d.index(i)]+=1
     print(f'#{t+1}')
-    for i in sorted(r):
-        print(e[i],end=' ')
+    for i,j in enumerate(r):
+        while j:print(d[i],end=' ');j-=1
