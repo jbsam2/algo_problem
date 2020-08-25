c963f60ce543056f5300207d104029e7d44edc50
KGB <kbsam2@gmail.com>
2020-08-25 22:59:40 +0900

    study

--- swea/D2/1954. 달팽이 숫자.py
+++ swea/D2/1954. 달팽이 숫자.py
@@ -1,9 +1,22 @@
 for t in range(int(input())):
-    n=int(input());b=[[0]*n for i in range(n)];dy=[0,1,0,-1];dx=[1,0,-1,0];d=r=c=0;l=1
-    while(l<=n*n):
-        b[r][c]=l;l+=1;nr=r+dy[d];nc=c+dx[d]
-        if 0<=nr<n and 0<=nc<n and b[nr][nc]==0:r,c=nr,nc
-        else:d=(d+1)%4;r+=dy[d];c+=dx[d]
+    n=int(input())
+    b=[[0]*n for i in range(n)]
+    dy=[0,1,0,-1]
+    dx=[1,0,-1,0]
+    direction=row=col=0
+    number=1
+    while number<=n*n:
+        b[row][col]=number
+        number+=1
+        new_row=row+dy[direction]
+        new_col=col+dx[direction]
+        if 0<=new_row<n and 0<=new_col<n and b[new_row][new_col]==0:
+            row,col=new_row,new_col
+        else:
+            direction=(direction+1)%4
+            row+=dy[direction]
+            col+=dx[direction]
+
     print(f'#{t+1}')
     for i in range(n):
         for j in range(n):print(b[i][j],end=' ')
--- swea/D2/SWEA_4861_ 회문_구진범.py
+++ swea/D2/SWEA_4861_ 회문_구진범.py
@@ -1,8 +1,16 @@
-def c(l,n,m):
-    for i in l:
-        for j in range(n+1-m):
-            if i[j:j+m]==i[j:j+m][::-1]:return i[j:j+m]
+def c(board,n,m):
+    for i in board:
+        for j in range(0,n+1-m):
+            if i[j:j+m]==i[j:j+m][::-1]:
+                return i[j:j+m]
+
 for t in range(int(input())):
-    n,m=map(int,input().split());l=[input()for _ in range(n)];l+=list(zip(*l));print('#{}'.format(t+1),end=' ')
-    for i in c(l,n,m):print(i,end='')
-    print()
+    n,m=map(int,input().split())
+    board=[input()for _ in range(n)]
+    for j in range(n):
+        tmp = ''
+        for i in range(n):
+            tmp += board[i][j]
+        board.append(tmp)
+    #board+=list(zip(*board)) 세로축을 쪼개서 col을 추가해준다.
+    print('#{} {}'.format(t+1,''.join(c(l,n,m))))
