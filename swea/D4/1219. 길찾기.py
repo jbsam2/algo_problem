7ffde1ef9b7fe931cb06ff128f719ea72b58dad3
KGB <kbsam2@gmail.com>
2020-08-26 19:04:03 +0900

    class

--- swea/D4/1219. 길찾기.py
+++ swea/D4/1219. 길찾기.py
@@ -1,11 +1,11 @@
 def dfs(s):
-    global visit,flag
-    visit[s]=1
+    global flag
+    visit.append(s)
     if s==99:flag=1;return
-    for i in range(100):
-        if b[s][i] and not visit[i]:dfs(i)
-
+    for i in b[s]:
+        if i not in visit:dfs(i)
+ 
 for t in range(10):
-    input();l=list(map(int,input().split()));b=[[0]*100 for _ in range(100)];visit=[0]*100;flag=0
-    for i in range(0,len(l),2):b[l[i]][l[i+1]]=1
+    input();l=list(map(int,input().split()));b=[[] for _ in range(100)];visit=[];flag=0
+    for i in range(0,len(l),2):b[l[i]].append(l[i+1])
     dfs(0);print(f'#{t+1}',flag)
