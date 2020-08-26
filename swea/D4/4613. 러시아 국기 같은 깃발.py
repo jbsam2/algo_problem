64e6972b879463dd72d89ae0fc91e0fbd54a2d62
KGB <kbsam2@gmail.com>
2020-08-26 18:59:40 +0900

    update

--- swea/D4/4613. 러시아 국기 같은 깃발.py
+++ swea/D4/4613. 러시아 국기 같은 깃발.py
@@ -1,5 +1,5 @@
 for t in range(int(input())):
-    n,m=map(int,input().split());f=[list(input())for _ in range(n)];c=[[m-i.count(c)for i in f]for c in 'WBR'];r=2**32
-    for i in range(n-2):
-        for j in range(i+1,n-1):r=min(sum(c[0][:i+1]+c[1][i+1:j+1]+c[2][j+1:]),r)
-    print(f'#{t+1}',r)
+    n,m=map(int,input().split());f=[list(input())for _ in range(n)];c=[[m-i.count(c)for i in f]for c in 'WBR'];r=1<<32
+    for i in range(n-2):
+        for j in range(i+1,n-1):r=min(sum(c[0][:i+1]+c[1][i+1:j+1]+c[2][j+1:]),r)
+    print(f'#{t+1}',r)
