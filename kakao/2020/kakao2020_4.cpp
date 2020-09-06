f0a81cf44bed6dbdf77908f3a03e311ca8ce9a95
KGB <kbsam2@gmail.com>
2020-09-06 15:26:07 +0900

    kakao

--- kakao/2020/kakao2020_4.cpp
+++ kakao/2020/kakao2020_4.cpp
@@ -1,11 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int tonum(char ch)
-{
-    if(ch=='?')return 26;
-    return ch-'a';
-}
+int tonum(char ch){return (ch=='?')?26:(ch-'a');}
 
 struct Trie
 {
@@ -13,44 +9,31 @@ struct Trie
     Trie* child[27];
     bool end;
     Trie():end(false),count(0)
-    {
-        memset(child,0,sizeof(child));
-    }
+    {memset(child,0,sizeof(child));}
     ~Trie()
-    {
-        for(int i=0;i<27;i++)
-            if(child[i])
-                delete child[i];
-    }
+    {for(int i=0;i<27;i++)if(child[i])delete child[i];}
     void insert(const char *key)
     {
         if(*key==0) end=true;
         else
         {
             int next=tonum(*key);
-            if(child[next]==NULL)
-                child[next]=new Trie();
+            if(child[next]==NULL)child[next]=new Trie();
             child[next]->insert(key+1);
         }
     }
     void find(const char *key)
     {
-        if(*key==0)
-        {
-            count++;
-            return;
-        }
+        if(*key==0){count++;return;}
         int next=tonum(*key);
-        if(child[26]!=NULL)
-            child[26]->find(key+1);
-        if(child[next]!=NULL)
-            child[next]->find(key+1);
+        if(child[26]!=NULL)child[26]->find(key+1);
+        if(child[next]!=NULL)child[next]->find(key+1);
     }
     int get_count(const char *key)
     {
         if(*key==0)return count;
         int next=tonum(*key);
-        if(child[next]==NULL) return 0;
+        if(child[next]==NULL)return 0;
         return child[next]->get_count(key+1);
     }
 };
@@ -59,31 +42,22 @@ vector<int> solution(vector<string> words, vector<string> queries)
 {
     vector<int> answer;
     Trie sol;
-    for(auto i:queries)
-        sol.insert(i.c_str());
-    for(auto i:words)
-        sol.find(i.c_str());
-    for(auto i:queries)
-        answer.push_back(sol.get_count(i.c_str()));
+    for(auto querie:queries)sol.insert(querie.c_str());
+    for(auto word:words)sol.find(word.c_str());
+    for(auto querie:queries)answer.push_back(sol.get_count(querie.c_str()));
     return answer;
 }
 
 
 
-#include <string>
-#include <vector>
-#include <unordered_map>
-
+#include <bits/stdc++.h>
 using namespace std;
 
 bool match(string &query,string &word)
 {
-    if(query.length()!=word.length())
-        return false;
-    for(int i=0;i<query.length();++i)
-        if(query[i]!='?'&&query[i]!=word[i])
-            return false;
-    return true;
+    if(query.length()!=word.length())return 0;
+    for(int i=0;i<query.length();i++)if(query[i]!='?'&&query[i]!=word[i])return 0;
+    return 1;
 }
 
 void create_map(vector<string> &words,unordered_map<string,int> &map)
@@ -101,20 +75,14 @@ void create_map(vector<string> &words,unordered_map<string,int> &map)
 vector<int> solution(vector<string> words,vector<string> queries)
 {
     unsigned long max_length=0;
-
-    for(string &word:words)
-        max_length=max(max_length,word.length());
-
+    for(string &word:words)max_length=max(max_length,word.length());
     vector<int> answer;
-
     if(max_length>1000)
     {
         for(string &query:queries)
         {
             int num_matched=0;
-            for(string &word:words)
-                if(match(query,word))
-                    num_matched++;
+            for(string &word:words)if(match(query,word))num_matched++;
             answer.push_back(num_matched);
         }
     }
@@ -122,8 +90,7 @@ vector<int> solution(vector<string> words,vector<string> queries)
     {
         unordered_map<string,int> map;
         create_map(words,map);
-        for(string &query:queries)
-            answer.push_back(map[query]);
+        for(string &query:queries)answer.push_back(map[query]);
     }
     return answer;
 }
---
+++ kakao/2020/kakao2020_4.py
@@ -0,0 +1,24 @@
+m={}
+def h(q,w):
+    if len(q)!=len(w):return 0
+    for i in range(len(q)):
+        if q[i]!='?' and q[i]!=w[i]:return 0
+    return 1
+def c(ws):
+    for w in ws:
+        for i in range(len(w)):k1=w[:i]+'?'*(len(w)-i);k2='?'*i+w[i:];m[k1]=m.get(k1,0)+1;m[k2]=m.get(k2,0)+1
+def solution(words, queries):
+    answer=[];ml=0
+    for w in words:ml=max(ml,len(w))
+    if ml>1000:
+        for q in queries:
+            t=0
+            for word in words:
+                if h(q,word):t+=1
+            answer+=[t]
+    else:
+        c(words)
+        for q in queries:
+            try:answer+=[m[q]]
+            except:answer+=[0]
+    return answer
