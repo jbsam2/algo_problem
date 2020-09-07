492229cd19ba6f66becd5d72397fd814924a4e1e
KGB <kbsam2@gmail.com>
2020-09-07 23:25:08 +0900

    study

--- swea/D4/4012. 요리사.py
+++ swea/D4/4012. 요리사.py
@@ -1,5 +1,5 @@
 from itertools import combinations as cb
 for t in range(int(input())):
-    n=int(input())//2;m=2*n;l=[[*map(int,input().split())]for _ in range(m)];a=[sum(i)+sum(j)for i,j in zip(l,zip(*l))];b=sum(a)//2;p=[]
-    for i in cb(a[:-1],n):p+=[abs(b-sum(i))]
+    n=int(input());l=[[*map(int,input().split())]for _ in range(n)];a=[sum(i)+sum(j)for i,j in zip(l,zip(*l))];b=sum(a)//2;p=[]
+    for i in cb(a,n//2):p+=[abs(b-sum(i))]
     print(f'#{t+1}',min(p))
