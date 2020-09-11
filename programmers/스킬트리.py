701dc365d441da24e4529db5ca17d8071941fe6a
KGB <kbsam2@gmail.com>
2020-09-11 23:42:46 +0900

    update

--- programmers/스킬트리.py
+++ programmers/스킬트리.py
@@ -5,6 +5,6 @@ def solution(skill, skill_trees):
         for j in range(len(i)):
             if i[j] in skill:l+=[i[j]]
         for j in range(len(l)):
-            if l[k]!=skill[k]:f=0;break
+            if l[j]!=skill[j]:f=0;break
         if f:answer+=1
     return answer
