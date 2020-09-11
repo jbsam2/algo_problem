c0ae361b29b2d99eb7837a2fd7d2c97cd40c38b5
구진범 <jinbeom.gu@facebook.com>
2020-09-11 23:32:54 +0900

    test ipad

--- programmers/3n타일링.py
+++ programmers/3n타일링.py
@@ -1,5 +1,5 @@
 m=1000000007
 def solution(n):
-    dp=[1,0,3]+[0]*(n+2)
-    for i in range(4,n+2,2):dp[i]=4*dp[i-2]%m-dp[i-4]%m
-    return dp[n]%m
+    d=[1,0,3]+[0]*(n+2)
+    for i in range(4,n+2,2):d[i]=4*d[i-2]%m-d[i-4]%m
+    return d[n]%m
