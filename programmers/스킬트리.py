c171b9c388165d4058f50dedec691c78b99e81b2
KGB <kbsam2@gmail.com>
2020-09-11 23:41:08 +0900

    made by ipad

---
+++ programmers/스킬트리.py
@@ -0,0 +1,10 @@
+def solution(skill, skill_trees):
+    answer=0
+    for i in skill_trees:
+        l=[];f=1
+        for j in range(len(i)):
+            if i[j] in skill:l+=[i[j]]
+        for j in range(len(l)):
+            if l[k]!=skill[k]:f=0;break
+        if f:answer+=1
+    return answer
