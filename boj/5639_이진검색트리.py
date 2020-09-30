fc923404246442c9b8aa793bede8655dbbea2584
KGB <kbsam2@gmail.com>
2020-09-30 21:57:02 +0900

    study

--- boj/5639_이진검색트리.py
+++ boj/5639_이진검색트리.py
@@ -1,6 +1,8 @@
 import sys
+sys.setrecursionlimit(100000)
 def sol(i,u):
-    p=l[i]
-    if u<p:return i
-    k=sol(sol(i+1,p),u);print(p);return k
-l=[*map(int,sys.stdin),9**9];sys.setrecursionlimit(100000);sol(0,9**8)
+    r=l[i]
+    if u<r:return i
+    k=sol(sol(i+1,r),u);print(r);return k
+l=[*map(int,sys.stdin),9**9]
+sol(0,9**8)
