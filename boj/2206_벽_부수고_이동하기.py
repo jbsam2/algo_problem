234c5084f56a40174980235f65bbd6529fed71db
KGB <kbsam2@gmail.com>
2020-10-06 21:25:11 +0900

    update

--- boj/2206_벽_부수고_이동하기.py
+++ boj/2206_벽_부수고_이동하기.py
@@ -4,10 +4,10 @@ c=[[[0]*2 for _ in range(m+1)]for _ in range(n+1)]
 b=[[int(i)for i in input()]for _ in range(n)]
 q=[];ret=-1
 q+=[(0,0,1,1)]
+c[0][0][1]=1
 while q:
     y,x,l,k=q.pop(0)
     if y==n-1 and x==m-1:ret=l;break
-    c[0][0][1]=1
     for i in range(4):
         Y=y+D[i];X=x+d[i]
         if 0<=X<m and 0<=Y<n:
