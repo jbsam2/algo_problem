685b8b7a779337d2d1a3828a16253b17a3b717bd
KGB <kbsam2@gmail.com>
2020-10-27 17:34:48 +0900

    update

--- swea/D5/1242. 암호코드 스캔.py
+++ swea/D5/1242. 암호코드 스캔.py
@@ -1,7 +1,7 @@
-c2={'211':0,'221':1,'122':2,'411':3,'132':4,'231':5,'114':6,'312':7,'213':8,'112':9}
+z={'211':0,'221':1,'122':2,'411':3,'132':4,'231':5,'114':6,'312':7,'213':8,'112':9}
 def r(a,b,c):m=min(a,b,c);a//=m;b//=m;c//=m;return str(a)+str(b)+str(c)              
 for t in range(int(input())):
-    n,m=map(int,input().split());ret=[];v=[];ans=0;d=[input() for _ in range(n)];h=['']*n
+    n,m=map(int,input().split());f=[];v=[];ans=0;d=[input() for _ in range(n)];h=['']*n
     for i in range(n):
         for j in range(m):h[i]+=bin(int(d[i][j],16))[2:].zfill(4)
     for y in range(n):
@@ -10,9 +10,9 @@ for t in range(int(input())):
             if a==0 and b==0 and h[y][x]=='1':c+=1
             elif c and a==0 and h[y][x]=='0':b+=1
             elif c and b and h[y][x]=='1':a+=1
-            if a and b and c and h[y][x]=='0':ret.append(c2[r(a,b,c)]);a=b=c=0
-            if len(ret)==8:
-                ret=ret[::-1];l=(ret[0]+ret[2]+ret[4]+ret[6])*3+(ret[1]+ret[3]+ret[5])+ret[7]
-                if l%10==0 and ret not in v:ans+=sum(ret)
-                v.append(ret);ret=[]
+            if a and h[y][x]=='0':f.append(z[r(a,b,c)]);a=b=c=0
+            if len(f)==8:
+                f=f[::-1];l=(f[0]+f[2]+f[4]+f[6])*3+(f[1]+f[3]+f[5])+f[7]
+                if l%10==0 and f not in v:ans+=sum(f)
+                v.append(f);f=[]
     print(f'#{t+1}',ans)
