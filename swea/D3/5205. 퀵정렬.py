b5b57e400f916a880a283b4a6733f406dfce7104
KGB <kbsam2@gmail.com>
2020-11-02 12:01:24 +0900

    update

--- swea/D3/5205. 퀵정렬.py
+++ swea/D3/5205. 퀵정렬.py
@@ -3,8 +3,8 @@ def qsort(arr,l,r):
 def partition(arr, l, r):
     p=(l+r)//2
     while l<r:
-        while(arr[l]<arr[p] and l<r):l+=1
-        while(arr[r]>=arr[p] and l<r):r-=1
+        while arr[l]<arr[p] and l<r:l+=1
+        while arr[r]>=arr[p] and l<r:r-=1
         if l<r:
             if l==p:p=r
             arr[l],arr[r]=arr[r],arr[l]
