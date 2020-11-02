c7fe8f682125ca11bbdb4366582b6b9f5c9c2e88
KGB <kbsam2@gmail.com>
2020-11-02 16:39:46 +0900

    update

--- swea/D3/SWEA_5205_퀵정렬_구진범.py
+++ swea/D3/SWEA_5205_퀵정렬_구진범.py
@@ -1,10 +1,10 @@
-def qsort(arr,l,r):
-    if l<r:p=partition(arr,l,r);qsort(arr,l,p-1);qsort(arr,p+1,r)
-def partition(arr, l, r):
-    p=(l+r)//2
+def qsort(l,r):
+    if l<r:p=partition(l,r);qsort(l,p-1);qsort(p+1,r)
+def partition(l,r):
+    p=l
     while l<r:
-        while arr[l]<arr[p] and l<r:l+=1
-        while arr[r]>=arr[p] and l<r:r-=1
+        while(arr[l]<arr[p] and l<r):l+=1
+        while(arr[r]>=arr[p] and l<r):r-=1
         if l<r:
             if l==p:p=r
             arr[l],arr[r]=arr[r],arr[l]
@@ -13,5 +13,5 @@ def partition(arr, l, r):
 for t in range(int(input())):
     n=int(input())
     arr=list(map(int, input().split()))
-    qsort(arr,0,n-1)
+    qsort(0,n-1)
     print(f'#{t+1}',arr[n//2])
